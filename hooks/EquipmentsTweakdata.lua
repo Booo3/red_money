17ddb310d6581b0f6d1c3749bdb5b511080c0701
unknown <patricstolz@gmail.com>
2020-07-24 13:12:31 +0200

    fixed #15

--- hooks/EquipmentsTweakdata.lua
+++ hooks/EquipmentsTweakdata.lua
@@ -1,12 +1,12 @@
 Hooks:PostHook(EquipmentsTweakData, "init", "red_money_equipments", function(self)
     self.specials.red_money_code = {
         sync_possession = true,
-		icon = "red_money_code",
+		icon = "equipment_barcode",
 		text_id = "red_money_code"
     }
     self.specials.red_money_scr_c4 = {
         sync_possession = true,
-		icon = "red_money_scr_c4",
+		icon = "pd2_c4",
 		text_id = "red_money_scr_c4"
     }
 end)
